78f882a3e0f74849a353503244a9861fdc9e5688
Ankur Anant <ankuranant99@gmail.com>
2020-07-28 20:29:04 +0530

    adding mirror + menu ui

---
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private List<Transform> targets;
+
+    [SerializeField] private Vector3 offset;
+
+    private void LateUpdate()
+    {
+
+        if (targets.Count == 0)
+            return;
+
+        Vector3 centerPoint = GetCenterPoint();
+        Vector3 offsetPosition = centerPoint + offset;
+        transform.position = offsetPosition;
+
+    }
+
+    Vector3 GetCenterPoint()
+    {
+        if (targets.Count == 1)
+            return targets[0].position;
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for(int i=0; i<targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+        return bounds.center;
+    }
+
+}
---
+++ Assets/Scripts/UI/MenuCanvas.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuCanvas : MonoBehaviour
+{
+    public void StartSinglePlayer()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void StartCouchCoop()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+
+    public void StartMultiplayer()
+    {
+        SceneManager.LoadSceneAsync(3);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
