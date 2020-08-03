eade7e596085f9a6fc7061f91ab214d3aa539ca9
Ankur Anant <ankuranant99@gmail.com>
2020-08-03 23:51:21 +0530

    player movement + salad mixer

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -9,16 +9,35 @@ public static class Constants
     public const KeyCode KeyBackward1 = KeyCode.S;
     public const KeyCode KeyLeft1 = KeyCode.A;
     public const KeyCode KeyRight1 = KeyCode.D;
+    public const KeyCode KeyRotateLeft1 = KeyCode.Q;
+    public const KeyCode KeyRotateRight1 = KeyCode.E;
+    public const KeyCode KeyPick1 = KeyCode.F;
+    public const KeyCode KeyDrop1 = KeyCode.R;
 
     // player2 controls
     public const KeyCode KeyForward2 = KeyCode.UpArrow;
     public const KeyCode KeyBackward2 = KeyCode.DownArrow;
     public const KeyCode KeyLeft2 = KeyCode.LeftArrow;
     public const KeyCode KeyRight2 = KeyCode.RightArrow;
+    public const KeyCode KeyRotateLeft2 = KeyCode.RightShift;
+    public const KeyCode KeyRotateRight2 = KeyCode.RightControl;
+    public const KeyCode KeyPick2 = KeyCode.Return;
+    public const KeyCode KeyDrop2 = KeyCode.Backspace;
 
     // common
     public const float powerupTime = 2.0f;
-    public const float normalSpeed = 10.0f;
+    public const float normalSpeed = 2.0f;
     public const float timePowerUp = 10.0f;
     public const float gameTime = 120.0f;
+
+    // raw vege
+    public const string Tomato = "Tomato";
+    public const string Brocolli = "Brocolli";
+    public const string Carrot = "Carrot";
+
+    // vege combo
+    public const string Tomolli = "Tomolli"; // tomato + brocolli
+    public const string Tarrot = "Tarrot"; // tomato + carrot
+    public const string Brorrocolli = "Brorrocolli"; // brocolli + Carrot
+
 }
--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -9,9 +9,14 @@ public class PlayerController : MonoBehaviour
 
     //public
     [SerializeField] TextMeshProUGUI timer;
+    [SerializeField] GameObject pickupLabelContainer;
+    [SerializeField] GameObject eye;
 
     private bool canPlayerMove = false;
     private float gameTime;
+    private float playerSpeed = Constants.normalSpeed;
+
+    private List<GameObject> Cart;
 
     private void Awake()
     {
@@ -21,33 +26,43 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
-        
+        Cart = new List<GameObject>(2);
     }
 
     void FixedUpdate()
     {
         PlayerMovement();
         UpdateTimer();
+        ShootRaycast();
     }
 
     private void PlayerMovement()
     {
         if (!canPlayerMove) return;
-        if(Input.GetKey(Constants.KeyForward1))
+
+        if (Input.GetKey(Constants.KeyForward1))
         {
-            this.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(Constants.normalSpeed, 0, 0);
+            transform.Translate(0, 0, 10 * Time.deltaTime);
         }
-        if (Input.GetKey(Constants.KeyBackward1))
+        if(Input.GetKey(Constants.KeyBackward1))
         {
-            this.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(-Constants.normalSpeed, 0, 0);
+            transform.Translate(0, 0, -10 * Time.deltaTime);
         }
         if (Input.GetKey(Constants.KeyLeft1))
         {
-            this.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, Constants.normalSpeed);
+            transform.Translate(-10 * Time.deltaTime, 0, 0);
         }
         if (Input.GetKey(Constants.KeyRight1))
         {
-            this.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, -Constants.normalSpeed);
+            transform.Translate(10 * Time.deltaTime, 0, 0);
+        }
+        if(Input.GetKey(Constants.KeyRotateLeft1))
+        {
+            transform.Rotate(0, transform.rotation.y - 5, 0);
+        }
+        if(Input.GetKey(Constants.KeyRotateRight1))
+        {
+            transform.Rotate(0, transform.rotation.y + 5, 0);
         }
     }
 
@@ -57,4 +72,50 @@ public class PlayerController : MonoBehaviour
         if (gameTime <= 0) canPlayerMove = false;
         timer.SetText((Math.Round(gameTime)).ToString() + "s");
     }
+
+    private void ShootRaycast()
+    {
+        RaycastHit hit;
+        Debug.Log("attempting");
+        Debug.DrawRay(eye.transform.position, eye.transform.forward * 10, Color.red);
+        if (Physics.Raycast(eye.transform.position, eye.transform.forward, out hit, 10))
+        {
+            Debug.Log(hit.transform.name);
+            Debug.Log(hit.transform.tag);
+            if (hit.transform.CompareTag("Vegetable"))
+            {
+                pickupLabelContainer.SetActive(true);
+                pickupLabelContainer.GetComponentInChildren<TextMeshProUGUI>().SetText($"F to pickup {hit.transform.name}");
+
+                // handle pickup and drop
+                HandleObject(hit.transform.gameObject);
+            }
+        } else
+        {
+            pickupLabelContainer.SetActive(false);
+        }
+    }
+
+    private void HandleObject(GameObject interactableObject)
+    {
+        if(Input.GetKeyDown(Constants.KeyPick1))
+        {
+            PickObject(interactableObject);
+        }
+    }
+
+    private void PickObject(GameObject interactableObject)
+    {
+        if(Cart.Count > 2)
+        {
+            return;
+        }
+
+        if(interactableObject.CompareTag("Vegetable"))
+        {
+            // item is raw vege
+
+        }
+    }
+
 }
